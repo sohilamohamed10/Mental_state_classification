cdc4f1f478ee362c936fda246eedb909d970a7af
Sohila mohamed <Sohila mohamed>
2022-02-06 23:05:16 +0200

    classifier-added

--- main.py
+++ main.py
@@ -35,31 +35,54 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier , AdaBoostClassifier , GradientBoostingClassifier
 
+
 dataset = pd.read_csv("mental-state.csv")	
 labels=dataset.iloc[1:,-1]
-data=dataset.iloc[1:,:-1]
+
+relaxed=dataset.loc[dataset['Label'] == 0.0]
+neutral=dataset.loc[dataset['Label'] == 1.0]
+concentrating=dataset.loc[dataset['Label'] == 2.0]
+p1=pd.concat([relaxed,neutral],axis=0)
+p2=pd.concat([relaxed,concentrating],axis=0)
+p3=pd.concat([concentrating,neutral],axis=0)
+classification_problems=[p1,p2,p3]
+
+relaxed_labels=dataset['Label'].loc[dataset['Label'] == 0.0]
+neutral_labels=dataset['Label'].loc[dataset['Label'] == 1.0]
+concentrating_labels=dataset['Label'].loc[dataset['Label'] == 2.0]
+labels1 = pd.concat([relaxed_labels,neutral_labels],axis=0)
+labels2 = pd.concat([relaxed_labels,concentrating_labels],axis=0)
+labels3 = pd.concat([concentrating_labels,neutral_labels],axis=0)
+all_labels=[labels1,labels2,labels3]
+
 kfold = KFold(n_splits= 10,shuffle= True, random_state=42)
-total_pred= []
-actual_labels=[]
 
-for train_Idx, test_Idx in kfold.split(data):
-	train_data=data.iloc[train_Idx]
-	test_data=data.iloc[test_Idx]
-	train_labels=labels.iloc[train_Idx]
-	test_labels=labels.iloc[test_Idx]
+for i in range (3):
+	total_pred= []
+	actual_labels=[]
+	data=classification_problems[i].iloc[1:,:-1]
+	label=all_labels[i].iloc[1:]
 
-	#train_selected  , test_selected  = Mutual_info()
-	classifier = RandomForestClassifier()
-	classifier.fit(train_data, train_labels)
-	pred_labels = classifier.predict(test_data)
-	total_pred.extend(pred_labels)
-	actual_labels.extend(test_labels)
+	for train_Idx, test_Idx in kfold.split(data):
 
-TN, FP, FN, TP = confusion_matrix(actual_labels, total_pred).ravel()
-acc =  (TP+TN) /(TP+FP+TN+FN)
-sen= TP/(TP+FN)
-spec= TN/(TN+FP)
-print(accuracy)
+		train_data=data.iloc[train_Idx]
+		test_data=data.iloc[test_Idx]
+		train_labels=label.iloc[train_Idx]
+		test_labels=label.iloc[test_Idx]
+
+		#train_selected  , test_selected  = Mutual_info()
+		classifier = RandomForestClassifier()
+		classifier.fit(train_data, train_labels)
+		pred_labels = classifier.predict(test_data)
+		total_pred.extend(pred_labels)
+		actual_labels.extend(test_labels)
+
+
+	TN, FP, FN, TP = confusion_matrix(actual_labels, total_pred).ravel()
+	acc =  (TP+TN) /(TP+FP+TN+FN)
+	sen= TP/(TP+FN)
+	spec= TN/(TN+FP)
+	print(acc)
 
 
 			
